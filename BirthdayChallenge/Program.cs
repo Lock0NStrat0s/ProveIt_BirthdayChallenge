31f4c8d29f3ef9a76773493c1592a106efc18bbc
Mohammad Jabir <mohammadj94@gmail.com>
2025-03-02 15:30:11 -0700

    completed: how old they are, time until next bday

--- BirthdayChallenge/Program.cs
+++ BirthdayChallenge/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace BirthdayChallenge
+﻿using System.Globalization;
+
+namespace BirthdayChallenge;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        //Console.Write("Enter your birthdate (mm/dd/yyyy): ");
+        //DateTime.TryParseExact(Console.ReadLine(), "MM/dd/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedDate);
+
+        DateTime parsedDate = new DateTime(2020, 1, 1);
+        DateTime currentDate = DateTime.Now;
+        int daysElapsed = (currentDate - parsedDate).Days;
+
+        Console.WriteLine($"Parsed: {parsedDate} Now: {currentDate}");
+        Console.WriteLine($"Elapsed days: {daysElapsed}");
+        Console.WriteLine($"You are {daysElapsed / 365} year(s), {(daysElapsed % 365) / 30} month(s), {(daysElapsed % 365 % 30) - 1} day(s).");
+
+        // months until next birthday
+        DateTime nextYearBirthDate = new DateTime(DateTime.Now.Year + 1, parsedDate.Month, parsedDate.Day);
+        int daysUntilNextBDay = (nextYearBirthDate - currentDate).Days;
+        Console.WriteLine($"Time until next birthday: {daysUntilNextBDay / 30} month(s), {(daysUntilNextBDay % 30) - 1} day(s).");
+
+        // weekends until their birthday
+
     }
 }
--- BirthdayChallenge/obj/Debug/net9.0/BirthdayChallenge.AssemblyInfo.cs
+++ BirthdayChallenge/obj/Debug/net9.0/BirthdayChallenge.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BirthdayChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abac5b1c76361cd728b7b85d22e1fe1d5578fd8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BirthdayChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BirthdayChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
